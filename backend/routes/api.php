659c10104f9c135dabeea8384adfb66536787b0d
GorudenTaiga <93591004+GorudenTaiga@users.noreply.github.com>
2023-05-18 00:29:52 +0700

    Add files via upload

---
+++ backend/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuthenticateController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/user', [AuthenticateController::class, 'index']);
+
+Route::post('/register', [AuthenticateController::class, 'register']);
