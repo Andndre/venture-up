bd5a9470f85a88a91af92da1c30309cd07984133
Andndre <agungandre687@gmail.com>
2023-05-31 11:09:10 +0800

    Cleanup

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Venture Up Indonesia</title>
+    <title>{{ $title }}</title>
 </head>
 
 <body>
-
+    @yield('content')
 </body>
 
 </html>
