1114f6c4f3252cda14f21ddaa7c91361536e6c91
Andndre <agungandre687@gmail.com>
2023-06-02 15:51:59 +0800

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,42 @@
 @php
     $title = 'Home';
     $navActive = '/';
+    
+    $kenapa = [
+        [
+            'title' => 'Berkualitas',
+            'desc' => 'Semua modul pembelajaran kami berkualitas dan mudah dipelajari',
+            'icon' => 'mdi-check-decagram',
+        ],
+        [
+            'title' => 'Mudah',
+            'desc' => 'Venture Up memiliki misi untuk mengedukasi orang awam dengan bahasa yang mudah dipahami',
+            'icon' => 'mdi-thumb-up',
+        ],
+        [
+            'title' => 'Ahli',
+            'desc' => 'Materi yang diberikan langsung dari ahlinya dan edukasi yang diberikan penting bagi masyarakat',
+            'icon' => 'mdi-tie',
+        ],
+    ];
+    
+    $materi = [
+        [
+            'title' => 'Pengetahuan Umum',
+            'desc' => 'Pengetahuan Umum Berupa skillset kita seperti marketing,sales, dan masih banyak lagi.',
+            'img' => 'pengetahuan-umum.png',
+        ],
+        [
+            'title' => 'Soft Skill',
+            'desc' => 'Softskill sangat penting bagi diri sendiri,dalam materi ini akan diajarkan berbagai macam softskill yang diperlukan.',
+            'img' => 'soft-skill.png',
+        ],
+        [
+            'title' => 'Keuangan dan Investasi',
+            'desc' => 'Materi nya itu mencakup cara mengatur keuangan dan akan difokuskan ke investasi seperti saham dan crypto.',
+            'img' => 'keuangan-dan-investasi.png',
+        ],
+    ];
 @endphp
 
 @extends('layouts.base')
@@ -46,27 +82,44 @@
                 <p>Keunggulan VentureUp yaitu kami membuat materi yang berkualitas
                     dan gampang di mengerti.</p>
             </div>
-            <div class="pt-3"></div>
         </div>
+        <div class="pt-3"></div>
         <div class="container px-4 mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-            <div class="space-y-3 text-center sm:text-left w-4/5 sm:w-full mx-auto">
-                <i class="mdi mdi-check-decagram text-6xl text-primary"></i>
-                <h3>Berkualitas</h3>
-                <p class="text-sm">Semua modul pembelajaran kami berkualitas dan mudah dipelajari</p>
-            </div>
-            <div class="space-y-3 text-center sm:text-left w-4/5 sm:w-full mx-auto">
-                <i class="mdi mdi-thumb-up text-6xl text-primary"></i>
-                <h3>Mudah</h3>
-                <p class="text-sm">Venture Up memiliki misi untuk mengedukasi orang awam dengan bahasa yang mudah dipahami
-                </p>
-            </div>
-            <div class="space-y-3 text-center sm:text-left w-4/5 sm:w-full mx-auto">
-                <i class="mdi mdi-tie text-6xl text-primary"></i>
-                <h3>Ahli</h3>
-                <p class="text-sm">Materi yang diberikan langsung dari ahlinya dan edukasi yang diberikan penting bagi
-                    masyarakat</p>
-            </div>
+            @foreach ($kenapa as $item)
+                <div class="space-y-3 text-center sm:text-left w-4/5 sm:w-full mx-auto">
+                    <i class="mdi {{ $item['icon'] }} text-6xl text-primary"></i>
+                    <h3>{{ $item['title'] }}</h3>
+                    <p class="text-sm"> {{ $item['desc'] }} </p>
+                </div>
+            @endforeach
         </div>
     </section>
     {{-- End Kenapa --}}
+
+    {{-- Materi Utama --}}
+    <section data-aos="fade-up" id="kenapa" class="min-h-screen">
+        <div class="mx-auto px-4 container max-w-2xl text-center mt-2 space-y-3">
+            <div class="space-y-3">
+                <h2>3 Materi Utama <span>VentureUp</span></h2>
+                <p>3 Materi Utama sangat penting bagi hidup anda dalam
+                    mengubah kualitas pendidikan Indonesia.</p>
+            </div>
+        </div>
+        <div class="py-3"></div>
+        <div class="container px-4 mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+            @foreach ($materi as $item)
+                <div class="space-y-3">
+                    <img class="aspect-[9/11] object-cover object-center"
+                        src="{{ asset('assets/landing/' . $item['img']) }}" alt="{{ $item['title'] }}">
+                    <h3>{{ $item['title'] }}</h3>
+                    <p class="text-sm"> {{ $item['desc'] }} </p>
+                </div>
+            @endforeach
+        </div>
+        <div class="py-4"></div>
+        <div class="text-center">
+            <a class="rounded-md bg-primary text-black font-bold px-2" href="/login">Login Sekarang</a>
+        </div>
+    </section>
+    {{-- End Materi Utama --}}
 @endsection
